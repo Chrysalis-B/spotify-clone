1e43f37875d701ac8f46685721f672ab80483132
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-06-18 16:01:58 +0200

    Add player layout

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,7 +1,17 @@
-import { Box } from '@chakra-ui/layout';
+import { Grid, GridItem } from '@chakra-ui/layout';
 
 const PlayerLayout = ({ children }) => {
-  return <Box>{children}</Box>;
+  return (
+    <Grid
+      height="100vh"
+      templateColumns="minmax(250px, 15%) 1fr"
+      templateRows="1fr 100px"
+    >
+      <GridItem overflow="scroll">sidbar</GridItem>
+      <GridItem overflow="scroll">{children}</GridItem>
+      <GridItem colSpan={2}>player</GridItem>
+    </Grid>
+  );
 };
 
 export default PlayerLayout;
