d934ef471bd935fedb548a3091cd22f67e1f8fa2
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-06-18 17:12:31 +0200

    Use named exports

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,17 +1,18 @@
 import { Grid, GridItem } from '@chakra-ui/layout';
+import { Sidebar } from './sidebar';
 
-const PlayerLayout = ({ children }) => {
+export const PlayerLayout = ({ children }) => {
   return (
     <Grid
       height="100vh"
       templateColumns="minmax(250px, 15%) 1fr"
       templateRows="1fr 100px"
     >
-      <GridItem overflow="scroll">sidbar</GridItem>
+      <GridItem overflow="scroll">
+        <Sidebar />
+      </GridItem>
       <GridItem overflow="scroll">{children}</GridItem>
       <GridItem colSpan={2}>player</GridItem>
     </Grid>
   );
 };
-
-export default PlayerLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Home = () => {
         <meta name="description" content="Spotify-like music player" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      Home
     </div>
   );
 };
