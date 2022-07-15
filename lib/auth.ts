1a1c56d26fe4c54c7563ff9bce4221fb6a60e9d2
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-07-15 15:30:32 +0300

    Add auth handler for validated routes

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
+import prisma from './prisma';
+
+export const validateRoute = handler => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
+
+    if (token) {
+      let user;
+
+      try {
+        const { id } = jwt.verfify(token, 'hello');
+        user = await prisma.user.findUnique({ where: { id } });
+        if (!user) throw new Error('Not real user');
+      } catch (err) {
+        res.status(401);
+        res.json({ error: 'Not Authorized' });
+        return;
+      }
+
+      return handler(req, res, user);
+    }
+
+    res.status(401);
+    res.json({ error: 'Not Authorized' });
+  };
+};
