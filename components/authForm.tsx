2c77bff0cccd4dfe7891952243e3ec9f9c9193f2
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-07-15 15:08:16 +0300

    Add signup / signin form and submission

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,14 +1,24 @@
 import { Box, Flex, Input, Button } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import NextImage from 'next/image';
 import { FC, useState } from 'react';
 import { useSWRConfig } from 'swr';
 import { auth } from '../lib/mutations';
 
-const AuthForm: FC<{ mode: string }> = ({ mode }) => {
-  const { email, setEmail } = useState('');
+const AuthForm: FC<{ mode: 'signin' | 'signup' }> = ({ mode }) => {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter;
+  const router = useRouter();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const user = await auth(mode, { email, password });
+    setIsLoading(false);
+    router.push('/');
+  };
 
   return (
     <Flex
@@ -18,11 +28,32 @@ const AuthForm: FC<{ mode: string }> = ({ mode }) => {
       bg="black"
       color="white"
     >
-      <Flex justify="center" align="center" height="100px">
-        hello
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
       <Flex justify="center" align="center" grow={1}>
-        form
+        <Box padding={10} bg="gray.900" borderRadius={3}>
+          <form onSubmit={handleSubmit}>
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={e => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={e => setPassword(e.target.value)}
+            />
+            <Button type="submit" colorScheme="teal" isLoading={isLoading}>
+              {mode}
+            </Button>
+          </form>
+        </Box>
       </Flex>
     </Flex>
   );
