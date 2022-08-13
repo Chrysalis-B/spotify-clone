a620888cddf05cd36f8645d57f17d620dc0cba58
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-08-13 15:16:30 +0200

    Capitalize file name

---
+++ components/PlayerLayout.tsx
@@ -0,0 +1,18 @@
+import { Grid, GridItem } from '@chakra-ui/layout';
+import { Sidebar } from './Sidebar/Sidebar';
+
+export const PlayerLayout = ({ children }) => {
+  return (
+    <Grid
+      height="100vh"
+      templateColumns="minmax(250px, 15%) 1fr"
+      templateRows="1fr 100px"
+    >
+      <GridItem overflow="hidden">
+        <Sidebar />
+      </GridItem>
+      <GridItem overflow="scroll">{children}</GridItem>
+      <GridItem colSpan={2}>player</GridItem>
+    </Grid>
+  );
+};
