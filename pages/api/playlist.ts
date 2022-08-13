15607e9cba43bcdd32fdc0b7895d1d75291273d1
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-08-13 17:04:38 +0200

    Add api route for getting playlists of a user

---
+++ pages/api/playlist.ts
@@ -0,0 +1,14 @@
+import prisma from '../../lib/prisma';
+import { validateRoute } from '../../lib/auth';
+
+export default validateRoute(async (_req, res, user) => {
+  const playlists = await prisma.playlist.findMany({
+    where: {
+      userId: user.id
+    },
+    orderBy: {
+      name: 'asc'
+    }
+  });
+  res.json(playlists);
+});
