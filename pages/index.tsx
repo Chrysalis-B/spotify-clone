679f3c65eadb4ade17d23cf59d620030cda7b384
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-08-13 18:34:19 +0200

    Add background gradient to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
+import { Box, Flex } from '@chakra-ui/layout';
 import Head from 'next/head';
+import { GradientBackground } from '../components/GradientBackground';
 
 const Home = () => {
+  const color = 'red';
   return (
-    <div>
+    <Box height="100%">
       <Head>
         <title>Spotify Clone</title>
         <meta name="description" content="Spotify-like music player" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Home
-    </div>
+      <GradientBackground color={color}>
+        <Flex bg={`${color}.600`} padding={6} align="end">
+          Heini
+        </Flex>
+        <div>Home page</div>
+      </GradientBackground>
+    </Box>
   );
 };
 
