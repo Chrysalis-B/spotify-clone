8a2ff4d1540b1372f76962318be057c26d49ecaf
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-08-13 17:05:09 +0200

    Handle responses in fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,3 +1,10 @@
+function handleFailedRequests(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
 export default function fetcher(url: string, data = undefined) {
   return fetch(`${window.location.origin}/api/${url}`, {
     method: data ? 'POST' : 'GET',
@@ -6,5 +13,10 @@ export default function fetcher(url: string, data = undefined) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
-  });
+  })
+    .then(handleFailedRequests)
+    .then(res => res.json())
+    .catch(err => {
+      throw err;
+    });
 }
