629887a555aa696525e136cd42c5a8d54e55a4ee
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-08-14 14:59:24 +0200

    Adjust gradient background

--- components/GradientBackground.tsx
+++ components/GradientBackground.tsx
@@ -4,8 +4,9 @@ export const GradientBackground = ({ color, children }) => {
   return (
     <Box
       height="100%"
+      overflowY="scroll"
       color="white"
-      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
+      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.9) 90%)`}
     >
       {children}
     </Box>
