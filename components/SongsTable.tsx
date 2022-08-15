25a0d278b578795cdfd7e1a9a972e2b5f296920e
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-08-15 17:45:41 +0200

    Expand songs table

--- components/SongsTable.tsx
+++ components/SongsTable.tsx
@@ -2,8 +2,25 @@ import { Box } from '@chakra-ui/layout';
 import { Table, Thead, Td, Tr, Tbody, Th, IconButton } from '@chakra-ui/react';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { AiOutlineClockCircle } from 'react-icons/ai';
+import { FC } from 'react';
+import { formatDate, formatTime } from '../lib/formatter';
 
-export const SongsTable = ({ songs }) => {
+interface SongProps {
+  id: number;
+  name: string;
+  duration: number;
+  createdAt: Date;
+  artist: {
+    id: number;
+    name: string;
+  };
+}
+
+interface SongListProps {
+  songs: SongProps[];
+}
+
+export const SongsTable: FC<SongListProps> = ({ songs }) => {
   return (
     <Box bg="transparent" color="white">
       <Box padding={9} marginBottom={4}>
@@ -27,6 +44,25 @@ export const SongsTable = ({ songs }) => {
               </Th>
             </Tr>
           </Thead>
+          <Tbody>
+            {songs.map((song, index) => (
+              <Tr
+                sx={{
+                  transition: 'all .3s',
+                  '&:hover': {
+                    bg: 'rgba(255,255,255,0.1)'
+                  }
+                }}
+                key={song.id}
+                cursor="pointer"
+              >
+                <Td>{index + 1}</Td>
+                <Td>{song.name}</Td>
+                <Td>{formatDate(song.createdAt)}</Td>
+                <Td>{formatTime(song.duration)}</Td>
+              </Tr>
+            ))}
+          </Tbody>
         </Table>
       </Box>
     </Box>
