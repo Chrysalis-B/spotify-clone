ed3c0108cf3ec1a02893c8d8bb6d3459a312cb0b
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-08-17 13:24:59 +0200

    Optimize player bar ui

--- components/PlayerBar.tsx
+++ components/PlayerBar.tsx
@@ -5,17 +5,17 @@ import Player from './Player';
 export const PlayerBar = () => {
   const songs = useStoreState((state: any) => state.activeSongs);
   const activeSong = useStoreState((state: any) => state.activeSong);
+
   return (
     <Box height="100%" width="100%" bg="gray.900" padding={2}>
-      <Flex align="center" justify="space-between">
-        <Box padding={4} color="white">
+      <Flex align="center">
+        <Box padding={4} color="white" flex="0 1 40%">
           <Text fontSize="lg">{activeSong?.name}</Text>
           <Text fontSize="sm">{activeSong?.artist?.name}</Text>
         </Box>
-        <Box flex="0 1 500px">
-          <Player songs={songs} activeSong={activeSong} />
+        <Box flex="0 1 40%">
+          {activeSong ? <Player songs={songs} activeSong={activeSong} /> : null}
         </Box>
-        <Box>Buttons</Box>
       </Flex>
     </Box>
   );
